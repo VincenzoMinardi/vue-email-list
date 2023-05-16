d8c51784d6595a643a89bc222122576e88781efa
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-16 15:40:03 +0200

    add: axios

--- assets/script-vue.js
+++ assets/script-vue.js
@@ -1,7 +1,16 @@
-const app = Vue.createApp({
+const root = Vue.createApp({
     data() {
         return {
-            
+            emailRandom: [],
         }
     },
-})
+
+    methods: {
+        getRandomEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(serverRandom => this.emailRandom = serverRandom.data.response)
+
+        }
+    },
+});
+
+root.mount('#app')
